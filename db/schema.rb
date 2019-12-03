1df07cf6d040d8ba83a0b925ba42ff754a3e179e
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2019-12-03 11:59:42 +0000

    fixing order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_174441) do
+ActiveRecord::Schema.define(version: 2019_12_03_111234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_174441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.string "url"
   end
 
   create_table "days", force: :cascade do |t|
