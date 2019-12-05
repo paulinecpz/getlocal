b0b89735f9510c875beb4768814c41b6414b0ea6
Pauline Coupez <coupez.pauline@gmail.com>
2019-12-05 17:23:31 +0000

    done

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,6 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
-    order.product_orders.destroy_all
+    current_user.product_orders.where(order: nil).destroy_all
   end
 end
